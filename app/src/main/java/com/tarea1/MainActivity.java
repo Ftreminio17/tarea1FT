94f8f28470cdb6f750d12c7ca15e88ed06c9187a
ftreminio17 <freym0417@gmail.com>
2022-10-26 17:43:24 -0600

    INISIO DE SESIÓN POR NÚMERO DE TELEFONO

--- app/src/main/java/com/tarea1/MainActivity.java
+++ app/src/main/java/com/tarea1/MainActivity.java
@@ -1,14 +1,132 @@
 package com.tarea1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText mEditTextNumberPhone;
+    Button mButtonSend;
+    TextView mTextViewResponse;
+    FirebaseAuth mAuth;
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        mEditTextNumberPhone = findViewById(R.id.editTextNumberT);
+        mButtonSend = findViewById(R.id.btnSend);
+        mTextViewResponse = findViewById(R.id.textViewResponse);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuth.setLanguageCode("es");
+        mButtonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    String phoneNumber = "+" + mEditTextNumberPhone.getText().toString();
+
+                    if(!phoneNumber.isEmpty()) {
+                        PhoneAuthOptions options =
+                                PhoneAuthOptions.newBuilder(mAuth)
+                                        .setPhoneNumber(phoneNumber)       // Phone number to verify
+                                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                                        .setActivity(MainActivity.this)                 // Activity (for callback binding)
+                                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                                        .build();
+                        PhoneAuthProvider.verifyPhoneNumber(options);
+                    } else {
+                        mTextViewResponse.setText("Ingrese su número de teléfono con el código del País");
+                        mTextViewResponse.setTextColor(Color.RED);
+                    }
+            }
+        });
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                iniciarSesion(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                mTextViewResponse.setText(e.getMessage());
+                mTextViewResponse.setTextColor(Color.RED);
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                mTextViewResponse.setText("El código de verificación fue enviado");
+                mTextViewResponse.setTextColor(Color.BLACK);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(MainActivity.this, VerificacionActivity.class);
+                        intent.putExtra("auth", s);
+                        startActivity(intent);
+                    }
+                },1000);
+            }
+        };
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            inicioActivityHome();
+        }
+    }
+
+
+
+    private void iniciarSesion(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    inicioActivityHome();
+                } else {
+                    mTextViewResponse.setText(task.getException().getMessage());
+                    mTextViewResponse.setTextColor(Color.RED);
+                }
+            }
+
+        });
+    }
+
+        private void inicioActivityHome() {
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+
 }
